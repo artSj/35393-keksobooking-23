c6843f642c879d5ec445dfba0777717b4af4f1e8
artSj <stray-white@ya.ru>
2021-07-12 22:00:38 +0700

    Добавляет валидацию формы

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import {renderCards} from './markup.js';
-import {deactivatePage, activatePage} from './form.js';
+import {deactivatePage, activatePage, validateForm} from './form.js';
 
 renderCards();
 deactivatePage();
 activatePage();
+validateForm();
